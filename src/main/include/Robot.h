60989e2ef9e55adc93124734358f9e3d7550e892
ElectroPanter <clhsrobotics@outlook.com>
2019-02-19 15:07:07 -0500

    inital commit

---
+++ src/main/include/Robot.h
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */                           
+/*------------------------------------------*/
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/Spark.h>
+//#include <frc/XboxController.h>
+#include <frc/Joystick.h>
+#include <frc/drive/MecanumDrive.h>
+class Robot : public frc::TimedRobot
+{
+  public:
+    void RobotInit() override;
+    void RobotPeriodic() override;
+    void AutonomousInit() override;
+    void AutonomousPeriodic() override;
+    void TeleopInit() override;
+    void TeleopPeriodic() override;
+    void TestPeriodic() override;
+
+  private:
+    frc::SendableChooser<std::string> m_chooser;
+    const std::string kAutoNameDefault = "Default";
+    const std::string kAutoNameCustom = "My Auto";
+    std::string m_autoSelected;
+    frc::Spark talonTL{4};
+    frc::Spark talonTR{2};
+    frc::Spark talonBL{3};
+    frc::Spark talonBR{1};
+    //frc::XboxController Controller{0};
+    frc::Joystick Controller{0};
+    frc::MecanumDrive mDrive{talonTL,talonBL,talonTR,talonBR};
+};
